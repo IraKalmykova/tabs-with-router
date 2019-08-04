e38f6a31155aae96f773cce7b15a78076c1166aa
Ira <kalmykova_ira@ukr.net>
2019-08-04 21:49:19 +0300

    Updates

---
+++ precache-manifest.837ea2833607903d15f4c50a414af0ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9acf5f5669339e0013e65cd62d88993c",
+    "url": "/tabs-with-router/index.html"
+  },
+  {
+    "revision": "511c7f6b3cee67b1184c",
+    "url": "/tabs-with-router/static/css/main.a0f22d1b.chunk.css"
+  },
+  {
+    "revision": "640b84aab9fcaae02add",
+    "url": "/tabs-with-router/static/js/2.d87dda62.chunk.js"
+  },
+  {
+    "revision": "511c7f6b3cee67b1184c",
+    "url": "/tabs-with-router/static/js/main.31976626.chunk.js"
+  },
+  {
+    "revision": "3513456d47be6d2df32f",
+    "url": "/tabs-with-router/static/js/runtime~main.8e076b95.js"
+  }
+]);
